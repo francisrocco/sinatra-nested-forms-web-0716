412aa784ab5ea194ba119a2509f8ac980d7ae416
Francis <roccofrancis@gmail.com>
2016-08-12 16:21:23 -0400

    Completed nested hashes but it is ugly

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:pirate][:name]
+    @weight = params[:pirate][:weight]
+    @height = params[:pirate][:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @all.clear
+  end
+
 end
